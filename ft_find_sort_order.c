c6271cb88e8f28dfa79cb8306423cd396c05f601
Vernon Olyvar <volyvar-@oa-f3.msk.21-school.ru>
2020-11-14 21:40:21 +0300

    norme: ft_find_sort_order.c

--- ft_find_sort_order.c
+++ ft_find_sort_order.c
@@ -1,50 +1,81 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_find_sort_order.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: volyvar- <volyvar-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/14 21:31:46 by volyvar-          #+#    #+#             */
+/*   Updated: 2020/11/14 21:39:14 by volyvar-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "ft_ls.h"
 
-void	ft_find_sort_order(t_dir_info* dir_info, t_info* info) {
-	if (!info->flags) {
+int		ft_check_first_sort_order(t_dir_info *dir_info, t_info *info)
+{
+	if (ft_strchr(info->flags, 'U'))
+	{
+		if (!ft_strchr(info->flags, 't') || ft_flag_order('t', 'U',
+															info->flags))
+			dir_info->sort_order = ORGN;
+		else
+			dir_info->sort_order = TMOD;
+		return (1);
+	}
+	if (ft_strchr(info->flags, 'f'))
+	{
+		if (!ft_strchr(info->flags, 't') || ft_flag_order('t', 'f',
+															info->flags))
+			dir_info->sort_order = ORGN;
+		else
+			dir_info->sort_order = TMOD;
+		return (1);
+	}
+	return (0);
+}
+
+int		ft_check_last_sort_order(t_dir_info *dir_info, t_info *info)
+{
+	if (ft_strchr(info->flags, 'S'))
+	{
+		if (ft_strchr(info->flags, 'r'))
+			dir_info->sort_order = RSIZE;
+		else
+			dir_info->sort_order = SIZE;
+		return (1);
+	}
+	if (ft_strchr(info->flags, 't'))
+	{
+		if (ft_strchr(info->flags, 'r'))
+			dir_info->sort_order = RTMOD;
+		else
+			dir_info->sort_order = TMOD;
+		return (1);
+	}
+	return (0);
+}
+
+void	ft_find_sort_order(t_dir_info *dir_info, t_info *info)
+{
+	if (!info->flags)
+	{
 		dir_info->sort_order = ALPH;
 		return ;
 	}
-
-	if (ft_strchr(info->flags, 'U')) {
-		if (!ft_strchr(info->flags, 't') || ft_flag_order('t', 'U', info->flags))
-			dir_info->sort_order = ORGN;
-		else
-			dir_info->sort_order = TMOD;
+	if (ft_check_first_sort_order(dir_info, info) == 1)
 		return ;
-	}
-
-	if (ft_strchr(info->flags, 'f')) {
-		if (!ft_strchr(info->flags, 't') || ft_flag_order('t', 'f', info->flags))
-			dir_info->sort_order = ORGN;
-		else
-			dir_info->sort_order = TMOD;
+	if (ft_check_last_sort_order(dir_info, info) == 1)
 		return ;
-	}
-
-	if (ft_strchr(info->flags, 'S')) {
-		if (ft_strchr(info->flags, 'r'))
-			dir_info->sort_order = 	RSIZE;
-		else
-			dir_info->sort_order = SIZE;
-		return ;
-	}
-
-	if (ft_strchr(info->flags, 't')) {
-		if (ft_strchr(info->flags, 'r'))
-			dir_info->sort_order = 	RTMOD;
-		else
-			dir_info->sort_order = TMOD;
-		return ;
-	}
-
-	if (ft_strchr(info->flags, 'r')) {
+	if (ft_strchr(info->flags, 'r'))
+	{
 		dir_info->sort_order = RALPH;
 		return ;
 	}
-	else {
-		dir_info->sort_order = 	ALPH;
+	else
+	{
+		dir_info->sort_order = ALPH;
 		return ;
 	}
-	dir_info->sort_order= ORGN;
+	dir_info->sort_order = ORGN;
 }
