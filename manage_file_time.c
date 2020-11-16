e03ecb87432873b011611381e789c36a5115bce4
Vernon Olyvar <volyvar-@oa-f3.msk.21-school.ru>
2020-11-16 12:44:46 +0300

    right time management

--- manage_file_time.c
+++ manage_file_time.c
@@ -6,7 +6,7 @@
 /*   By: volyvar- <volyvar-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/15 15:14:35 by volyvar-          #+#    #+#             */
-/*   Updated: 2020/11/15 15:17:10 by volyvar-         ###   ########.fr       */
+/*   Updated: 2020/11/16 12:42:36 by volyvar-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,8 @@ void	ft_free_after_split(char **arr)
 
 void	ft_find_year_or_time(char **separate_date,
 								char **separate_time,
-								t_file_info *file_info)
+								t_file_info *file_info,
+								time_t mtime)
 {
 	time_t	now_time;
 	char	*now_time_str;
@@ -34,19 +35,25 @@ void	ft_find_year_or_time(char **separate_date,
 	char	*now_year;
 	char	*file_year;
 
-	now_time = time(&now_time);
+	now_time = time(NULL);
 	now_time_str = ft_strdup(ctime(&now_time));
 	separate_now_time = ft_strsplit(now_time_str, ' ');
 	ft_strdel(&now_time_str);
 	now_year = ft_strsub(separate_now_time[4], 0, 4);
 	file_year = ft_strsub(separate_date[4], 0, ft_strlen(separate_date[4]) - 1);
-	if (!ft_strcmp(now_year, file_year))
+	// ft_printf("file: %ld now: %ld deff: %ld\n", mtime, now_time, now_time - mtime);
+	// ft_printf("sytki: %ld | month: %ld\n", (now_time-mtime) / (60*60*24) <= 181, (now_time-mtime) / (60*60*24*30));
+
+	if (((now_time-mtime) / (60*60*24)) < 182 && (now_time-mtime) >= 0)
 	{
 		file_info->time->hour = ft_strdup(separate_time[0]);
 		file_info->time->min = ft_strdup(separate_time[1]);
 	}
-	else
+	else 
 		file_info->time->year = ft_strdup(file_year);
+
+
+
 	ft_strdel(&now_year);
 	ft_strdel(&file_year);
 	ft_free_after_split(separate_now_time);
@@ -69,7 +76,7 @@ void	ft_print_time_modify(struct stat status, t_file_info *file_info)
 	separate_time = ft_strsplit(separate_date[3], ':');
 	file_info->time->month = ft_strdup(separate_date[1]);
 	file_info->time->day = ft_strdup(separate_date[2]);
-	ft_find_year_or_time(separate_date, separate_time, file_info);
+	ft_find_year_or_time(separate_date, separate_time, file_info, mtime);
 	ft_free_after_split(separate_time);
 	ft_free_after_split(separate_date);
 	free(separate_time);
